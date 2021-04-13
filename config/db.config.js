076930a002791eb18e134c45f23e22a1e7b14b84
umakant dubey <happydubey09@gmail.com>
2021-04-13 08:24:24 +0530

    credds removed

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "freedb.tech",
-    USER: "freedbtech_umakant",
-    PASSWORD: "umakant",
-    DB: "freedbtech_product"
+    HOST: "",
+    USER: "",
+    PASSWORD: "",
+    DB: ""
   };
